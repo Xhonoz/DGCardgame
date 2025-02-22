61f54bf258e8cc0a2114b2e5f093f1994d2cd1bc
Xhonoz <markus.loland@hotmail.com>
2025-02-22 11:49:04 +0100

    Add undo feature

--- src/components/SwipeGallery/index.tsx
+++ src/components/SwipeGallery/index.tsx
@@ -19,9 +19,14 @@ interface SwipeGalleryProps {
   deck: card[];
 }
 
+type Action = {
+  type: string;
+  card: card;
+};
+
 export const SwipeGallery = ({ deck }: SwipeGalleryProps) => {
-  console.log(deck.length);
   const [cards, setCards] = useState<card[]>([]);
+  const [actionStack, setActionStack] = useState<Action[]>([]);
 
   const [activeIndex, setActiveIndex] = useState(-1);
   const containerRef = useRef<HTMLDivElement>(null);
@@ -35,10 +40,7 @@ export const SwipeGallery = ({ deck }: SwipeGalleryProps) => {
   }, [scrollActiveIndex]);
 
   const move = (newActiveElement: number) => {
-    console.log(cards);
-    console.log(`move to index : ${newActiveElement}`);
     const currentNode = containerRef?.current;
-    console.log(currentNode?.childNodes.length);
     if (!currentNode) {
       return;
     }
@@ -67,9 +69,17 @@ export const SwipeGallery = ({ deck }: SwipeGalleryProps) => {
     setIsDrawing(true);
     const newCard = deck[Math.floor(Math.random() * deck.length)];
     setCards((prevCards) => [...prevCards, newCard]);
+    setActionStack((prevStack) => [
+      ...prevStack,
+      {
+        type: "draw",
+        card: newCard,
+      },
+    ]);
   };
 
   useEffect(() => {
+    console.log(cards);
     if (isDrawing) {
       setIsDrawing(false);
       if (cards.length > 0) {
@@ -78,8 +88,43 @@ export const SwipeGallery = ({ deck }: SwipeGalleryProps) => {
     }
   }, [cards, isDrawing]);
 
+  const undoAction = () => {
+    if (actionStack.length > 0) {
+      const lastAction = actionStack[actionStack.length - 1];
+      if (lastAction.type === "draw") {
+        setCards((cards) => {
+          // Find the index of the first card with the given name
+          const indexToRemove = cards.findIndex(
+            (card) => card.name === lastAction.card.name
+          );
+
+          // If the card is found, remove it
+          if (indexToRemove !== -1) {
+            return [
+              ...cards.slice(0, indexToRemove), // All cards before the one to remove
+              ...cards.slice(indexToRemove + 1), // All cards after the one to remove
+            ];
+          }
+
+          // If the card is not found, return the original array
+          return cards;
+        });
+
+        if (cards.length === 1) {
+          // play last card
+          setActiveIndex(-1);
+        }
+      }
+      if (lastAction.type === "play") {
+        setCards((prevCards) => [...prevCards, lastAction.card]);
+      }
+      setActionStack((prevStack) => prevStack.slice(0, -1)); // Remove the last action
+    }
+  };
+
   const handlePlay = () => {
     setPlayAnimation(true); // Trigger the animation
+    const playedCard = cards[activeIndex];
     setTimeout(() => {
       setPlayAnimation(false);
       setCards((cards) =>
@@ -89,11 +134,28 @@ export const SwipeGallery = ({ deck }: SwipeGalleryProps) => {
         // play last card
         setActiveIndex(-1);
       }
-    }, 500); // Reset after animation completes
+    }, 500);
+    setActionStack((prevStack) => [
+      ...prevStack,
+      {
+        type: "play",
+        card: playedCard,
+      },
+    ]); // Reset after animation completes
   };
 
   return (
     <div className="gallery">
+      <div>
+        <Button
+          className="undoButton"
+          onClick={() => {
+            undoAction();
+          }}
+        >
+          Undo
+        </Button>
+      </div>
       <div className="gallery-container" ref={containerRef}>
         {cards.map((card, index) => (
           <div key={index} className="gallery-container-item">
